59a6e240a5cc836fff6ec7c444d50faa84868203
Toast25 <94575226+Toast25@users.noreply.github.com>
2022-05-24 20:02:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function closet() {
+window.location = "closet.html";
+}
+
+function guest_r() {
+window.location = "guest_room.html";
+}
+
+function living_r() {
+window.location = "Living_Room.html";
+}
+
+function my_r() {
+ window.location = "my_room.html";
+  }
+
+function bath_r() {
+window.location = "restroom.html";
+}
+function back() {
+    window.location = "index.html";
+}
